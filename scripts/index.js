420729fac3e98d3f61f61537ded01350060c8e59
elitsuri <zelitesuri@gmail.com>
2022-09-19 16:17:50 +0300

    19.09.2022, 16:15

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,19 @@
   image_interest.addEventListener('mouseout', imageInterestMouseOut, false);
 })(window, window.document);
 
+const cursor = document.querySelector('.cursor');
 
+document.addEventListener('mousemove', e => {
+        cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+        })
+
+document.addEventListener('click', () => {
+        cursor.classList.add("expand");
+
+        setTimeout(() => {
+                cursor.classList.remove("expand");
+            }, 500)
+        })
 function imageInterestMouseOver(event)
 {
     event = event || window.event;
